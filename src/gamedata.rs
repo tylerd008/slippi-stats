702de432f927a3988cb0b20356710750cc9994b4
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-05-13 18:56:14 -0400

    cleanup and rustfmt

--- src/gamedata.rs
+++ src/gamedata.rs
@@ -1,13 +1,12 @@
-
-use peppi::game::{Game, Frames, Port};
+use peppi::game::{Frames, Game, Port};
 use std::fmt;
 #[derive(Debug)]
-pub struct GameResults{
-    results: Vec<GameResult>
+pub struct GameResults {
+    results: Vec<GameResult>,
 }
 
 #[derive(Debug)]
-pub struct GameResult{
+pub struct GameResult {
     player_char: usize,
     opponent_char: usize,
     stage: usize,
@@ -15,7 +14,7 @@ pub struct GameResult{
 }
 
 #[derive(Debug)]
-enum MatchResult{
+enum MatchResult {
     Victory(MatchEndType),
     Loss(MatchEndType),
     EarlyEnd(usize),
@@ -25,11 +24,11 @@ enum MatchResult{
 #[derive(Debug)]
 enum MatchEndType {
     Stocks,
-    Timeout
+    Timeout,
 }
 
 #[derive(Debug)]
-pub enum GameParseError{
+pub enum GameParseError {
     CorruptedCharData(usize),
     CorruptedStageData(usize),
     CorruptedPlayerData,
@@ -37,10 +36,10 @@ pub enum GameParseError{
     IncorrectPlayerCount,
 }
 
-impl GameResults{
-    pub fn new() -> Self{
-        Self{
-            results: Vec::new()
+impl GameResults {
+    pub fn new() -> Self {
+        Self {
+            results: Vec::new(),
         }
     }
 
@@ -49,15 +48,15 @@ impl GameResults{
     }
 }
 
-impl GameResult{
-    pub fn parse_game(game: Game) -> Result<Self, GameParseError>{
-        let player_num =  match get_player_num(&game){
+impl GameResult {
+    pub fn parse_game(game: Game) -> Result<Self, GameParseError> {
+        let player_num = match get_player_num(&game) {
             Some(num) => num,
             None => {
                 return Err(GameParseError::CorruptedPlayerData);
             }
         };
-        let match_result = match get_match_result(&game, player_num){
+        let match_result = match get_match_result(&game, player_num) {
             Ok(game_res) => game_res,
             Err(e) => {
                 return Err(e);
@@ -65,44 +64,51 @@ impl GameResult{
         };
 
         let player_char = get_char(&game, player_num)?;
-        let opponent_char = get_char(&game, 1- player_num)?;
-        
+        let opponent_char = get_char(&game, 1 - player_num)?;
+
         let stage = game.start.stage.0 as usize;
 
-        if stage == 0 || stage == 1 || stage == 21 || stage > 32{
+        if stage == 0 || stage == 1 || stage == 21 || stage > 32 {
             return Err(GameParseError::CorruptedStageData(stage));
         }
 
-        Ok(
-            Self{
-                player_char,
-                opponent_char,
-                stage,
-                match_result,
-            }
-        )
+        Ok(Self {
+            player_char,
+            opponent_char,
+            stage,
+            match_result,
+        })
     }
 }
 
 impl fmt::Display for GameResult {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        write!(f, "{} vs {} on {}. {}", num_to_char(self.player_char), num_to_char(self.opponent_char), num_to_stage(self.stage), self.match_result)
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} vs {} on {}. {}",
+            num_to_char(self.player_char),
+            num_to_char(self.opponent_char),
+            num_to_stage(self.stage),
+            self.match_result
+        )
     }
 }
 
-impl fmt::Display for MatchResult{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+impl fmt::Display for MatchResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             MatchResult::Victory(endtype) => write!(f, "Won by {}.", endtype),
             MatchResult::Loss(endtype) => write!(f, "Lost by {}.", endtype),
-            MatchResult::EarlyEnd(player_num) => write!(f, "Match ended early by player {}.", player_num),
+            MatchResult::EarlyEnd(player_num) => {
+                write!(f, "Match ended early by player {}.", player_num)
+            }
             MatchResult::Tie => write!(f, "Ended in a tie."),
         }
     }
 }
 
 impl fmt::Display for MatchEndType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             MatchEndType::Stocks => write!(f, "stocks"),
             MatchEndType::Timeout => write!(f, "timeout"),
@@ -110,24 +116,23 @@ impl fmt::Display for MatchEndType {
     }
 }
 
-fn get_player_num(game: &Game) -> Option<usize>{//rewrite this to return player number, not the player struct
-	let players = game.metadata.players.as_ref().unwrap();
-	let p2_md = players.get(1).unwrap();
-	let p2_np_code = match &p2_md.netplay{
-		Some(c) => &c.code,
-		None => {
-			return None;
-		}
-	};
-	if p2_np_code == "FLOS#497"{
-		Some(1)
-	} else {
-		Some(0)
-	}
+fn get_player_num(game: &Game) -> Option<usize> {
+    let players = game.metadata.players.as_ref().unwrap();
+    let p2_md = players.get(1).unwrap();
+    let p2_np_code = match &p2_md.netplay {
+        Some(c) => &c.code,
+        None => {
+            return None;
+        }
+    };
+    if p2_np_code == "FLOS#497" {
+        Some(1)
+    } else {
+        Some(0)
+    }
 }
 
-fn get_match_result(game: &Game, player_num: usize) -> Result<MatchResult, GameParseError>{//maybe redo this getting the game end data from the metadata to account for LRAStart
-
+fn get_match_result(game: &Game, player_num: usize) -> Result<MatchResult, GameParseError> {
     let frames = &game.frames;
     let data = match frames {
         Frames::P2(d) => d,
@@ -135,41 +140,41 @@ fn get_match_result(game: &Game, player_num: usize) -> Result<MatchResult, GameP
             return Err(GameParseError::IncorrectPlayerCount);
         }
     };
-	let pp_lf = &data[data.len()-1].ports[player_num];//player port last frame
+    let pp_lf = &data[data.len() - 1].ports[player_num]; //player port last frame
 
-	let op_lf = &data[data.len()-1].ports[1 - player_num];//opponent player last frame
+    let op_lf = &data[data.len() - 1].ports[1 - player_num]; //opponent player last frame
 
-	let p_end_stocks = pp_lf.leader.post.stocks;
-	let o_end_stocks = op_lf.leader.post.stocks;
+    let p_end_stocks = pp_lf.leader.post.stocks;
+    let o_end_stocks = op_lf.leader.post.stocks;
 
     let ev20 = game.end.v2_0.as_ref().unwrap();
 
-    if ev20.lras_initiator != None{
+    if ev20.lras_initiator != None {
         let asdf = ev20.lras_initiator.unwrap();
-            match asdf {
-                Port::P1 => {
-                    return Ok(MatchResult::EarlyEnd(1));
-                }
-                Port::P2 => {
-                    return Ok(MatchResult::EarlyEnd(2));
-                }
-                Port::P3 => {
-                    return Ok(MatchResult::EarlyEnd(3));
-                }
-                Port::P4 => {
-                    return Ok(MatchResult::EarlyEnd(4));
+        match asdf {
+            Port::P1 => {
+                return Ok(MatchResult::EarlyEnd(1));
+            }
+            Port::P2 => {
+                return Ok(MatchResult::EarlyEnd(2));
+            }
+            Port::P3 => {
+                return Ok(MatchResult::EarlyEnd(3));
+            }
+            Port::P4 => {
+                return Ok(MatchResult::EarlyEnd(4));
             }
         }
     }
 
-	if p_end_stocks > o_end_stocks{
-		Ok(MatchResult::Victory(MatchEndType::Stocks))
-	} else if p_end_stocks < o_end_stocks {
-		Ok(MatchResult::Loss(MatchEndType::Stocks))
-	} else {
-		if pp_lf.leader.post.damage < op_lf.leader.post.damage {
-            Ok(MatchResult::Victory(MatchEndType::Timeout))     
-	    } else if pp_lf.leader.post.damage > op_lf.leader.post.damage {
+    if p_end_stocks > o_end_stocks {
+        Ok(MatchResult::Victory(MatchEndType::Stocks))
+    } else if p_end_stocks < o_end_stocks {
+        Ok(MatchResult::Loss(MatchEndType::Stocks))
+    } else {
+        if pp_lf.leader.post.damage < op_lf.leader.post.damage {
+            Ok(MatchResult::Victory(MatchEndType::Timeout))
+        } else if pp_lf.leader.post.damage > op_lf.leader.post.damage {
             Ok(MatchResult::Loss(MatchEndType::Timeout))
         } else {
             Ok(MatchResult::Tie)
@@ -177,21 +182,23 @@ fn get_match_result(game: &Game, player_num: usize) -> Result<MatchResult, GameP
     }
 }
 
-fn get_char(game: &Game, player: usize) -> Result<usize, GameParseError>{
+fn get_char(game: &Game, player: usize) -> Result<usize, GameParseError> {
     let char_num = match game.start.players.get(player) {
         Some(character) => character,
         None => {
             return Err(GameParseError::EmptyCharData);
         }
-    }.character.0 as usize;
+    }
+    .character
+    .0 as usize;
 
-    if char_num > 26{
-         return Err(GameParseError::CorruptedCharData(char_num));
+    if char_num > 26 {
+        return Err(GameParseError::CorruptedCharData(char_num));
     }
     Ok(char_num)
 }
 
-fn num_to_char(char_num: usize) -> String{
+fn num_to_char(char_num: usize) -> String {
     match char_num {
         0 => String::from("Captain Falcon"),
         1 => String::from("Donkey Kong"),
@@ -219,11 +226,11 @@ fn num_to_char(char_num: usize) -> String{
         23 => String::from("Roy"),
         24 => String::from("Pichu"),
         25 => String::from("Ganondorf"),
-        _ => unreachable!()//any other value is errored out before this fn is ever called
+        _ => unreachable!(), //any other value is errored out before this fn is ever called
     }
 }
 
-fn num_to_stage(stage_num: usize) -> String{
+fn num_to_stage(stage_num: usize) -> String {
     match stage_num {
         2 => String::from("Fountain of Dreams"),
         3 => String::from("Pokémon Stadium"),
@@ -248,13 +255,13 @@ fn num_to_stage(stage_num: usize) -> String{
         23 => String::from("Poké Floats"),
         24 => String::from("Big Blue"),
         25 => String::from("Icicle Mountain"),
-        26 => String::from("Icetop"),//?
+        26 => String::from("Icetop"), //?
         27 => String::from("Flat Zone"),
         28 => String::from("Dream Land N64"),
         29 => String::from("Yoshi's Island N64"),
         30 => String::from("Kongo Jungle N64"),
         31 => String::from("Battlefield"),
         32 => String::from("Final Destination"),
-        _ => unreachable!()//any other value is errored out before this fn is ever called
+        _ => unreachable!(), //any other value is errored out before this fn is ever called
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod gamedata;
 
-use gamedata::{GameResults, GameResult};
+use gamedata::{GameResult, GameResults};
 
 use std::fs::File;
 
@@ -9,29 +9,32 @@ use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-	let p = PathBuf::from("C:/Users/Flossy/Documents/Slippi");
+    let p = PathBuf::from("C:/Users/Flossy/Documents/Slippi");
 
-	let mut results = GameResults::new();
+    let mut results = GameResults::new();
 
-	for entry in fs::read_dir(p).unwrap() {
-		let path = entry.unwrap().path();
-		let game = match peppi::game(&mut File::open(&path).unwrap(), Some(parse::Opts{skip_frames: false})){
-			Ok(val) => val,
-			Err(e) => {
-				println!("Error {:?}, when parsing game: {:?}", e, path);
-				continue;
-			}
-		};
+    for entry in fs::read_dir(p).unwrap() {
+        let path = entry.unwrap().path();
+        let game = match peppi::game(
+            &mut File::open(&path).unwrap(),
+            Some(parse::Opts { skip_frames: false }),
+        ) {
+            Ok(val) => val,
+            Err(e) => {
+                println!("Error {:?}, when parsing game: {:?}", e, path);
+                continue;
+            }
+        };
 
-		let result = match GameResult::parse_game(game) {
-			Ok(g) => g,
-			Err(e) => {
-				println!("Error when parsing game result: {:?}", e);
-				continue;
-			}
-		};
+        let result = match GameResult::parse_game(game) {
+            Ok(g) => g,
+            Err(e) => {
+                println!("Error when parsing game result: {:?}", e);
+                continue;
+            }
+        };
 
-		println!("{}", &result);
-		results.add_game(result);
-	}
+        println!("{}", &result);
+        results.add_game(result);
+    }
 }
