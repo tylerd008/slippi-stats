b969d5c960ce8b736afabf2afb4e2ba0e40ac8d6
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-06-21 09:45:49 -0400

    add placeholder text for wip commands

--- src/text.rs
+++ src/text.rs
@@ -3,6 +3,7 @@ pub const CHARACTER_HELP_TEXT: &str =
 pub const STAGE_HELP_TEXT: &str = "Gives commands for retrieving stats about a specifed stage.";
 pub const MATCHUP_HELP_TEXT: &str = "Commands for finding out stats regarding a given matchup.";
 pub const LAST_HELP_TEXT: &str = "Prints the results of the most recent specified number of games.";
+pub const PLAYER_HELP_TEXT: &str = "Commands for getting overall stats about the player.";
 pub const END_HELP_TEXT: &str = "Ends the program.";
 
 pub const C_WINRATE_HELP_TEXT: &str = "Get the overall winrate for the given character.";
@@ -14,3 +15,5 @@ pub const S_CHARACTERS_HELP_TEXT: &str =
     "Prints the winrate as every character on the stage that there is data for.";
 pub const S_MATCHUPS_HELP_TEXT: &str =
     "Gives the winrate versus every character that there is data for on the given stage.";
+
+pub const PLACEHOLDER_TEXT: &str = "todo: write text for this command";
