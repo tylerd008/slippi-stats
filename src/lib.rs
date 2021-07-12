195544e5e631e90812aa996c06b17c89ea37e500
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-11 21:13:52 -0400

    Write tests using `Character` and `Stage`

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,34 @@
+mod character;
 pub mod input;
+mod parsable_enum;
 mod playerdata;
+mod stage;
 mod text;
 
 #[cfg(test)]
 mod tests {
-    use crate::playerdata::ArgType;
+    use crate::character::Character;
+    use crate::stage::Stage;
+    use std::convert::TryFrom;
+    use std::str::FromStr;
     #[test]
-    fn char_parse() {
-        let arg: ArgType = "falcon".parse().unwrap();
-        let parsed = ArgType::Character(0);
-        assert_eq!(parsed, arg);
+    fn char_parse_from_usize() {
+        let char_from_usize = Character::try_from(0).unwrap();
+        assert_eq!(Character::CaptainFalcon, char_from_usize);
     }
     #[test]
-    fn char_parse2() {
-        let arg: ArgType = "captain falcon".parse().unwrap();
-        let parsed = ArgType::Character(0);
-        assert_eq!(parsed, arg);
+    fn char_parse_from_str() {
+        let char_from_str = Character::from_str("captain falcon").unwrap();
+        assert_eq!(Character::CaptainFalcon, char_from_str);
     }
     #[test]
-    fn char_parse3() {
-        let parsed1: ArgType = "captain falcon".parse().unwrap();
-        let parsed2: ArgType = "falcon".parse().unwrap();
-        assert_eq!(parsed1, parsed2);
+    fn stage_parse_from_usize() {
+        let stage_from_usize = Stage::try_from(2).unwrap();
+        assert_eq!(Stage::FountainOfDreams, stage_from_usize);
     }
     #[test]
-    fn stage_parse() {
-        let arg: ArgType = "fod".parse().unwrap();
-        let parsed = ArgType::Stage(2);
-        assert_eq!(arg, parsed);
+    fn stage_parse_from_usize_nonexistent_stage() {
+        let stage_from_usize = Stage::try_from(0);
+        assert_eq!(Err(()), stage_from_usize);
     }
 }
