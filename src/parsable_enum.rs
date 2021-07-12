b3165f41d7a2758876980960d1cf8288b67b78d4
Flossy271 <77032854+Flossy271@users.noreply.github.com>
2021-07-11 21:12:25 -0400

    Create `parsable_enum` macro

---
+++ src/parsable_enum.rs
@@ -0,0 +1,54 @@
+use crate::playerdata::GameData;
+use std::convert::TryFrom;
+use std::fmt::Display;
+use std::str::FromStr;
+
+pub trait Parsable: FromStr + Display + TryFrom<usize> {}
+pub trait UnnamedTrait {
+    //name this
+    fn condition(&self, game: &GameData) -> bool; //come up with more descriptive name
+}
+
+#[macro_export]
+macro_rules! parsable_enum {
+    ($vis:vis enum $name:ident{
+        $($disp_name:literal; $($alias:literal,)* => $val:ident = $num_val:expr,)*
+    }) => {
+        #[derive(Clone, Copy, Debug, PartialEq)]
+        $vis enum $name {
+            $($val = $num_val,)*
+        }
+
+        impl crate::parsable_enum::Parsable for $name{}
+
+        impl std::str::FromStr for $name {
+            type Err = ();
+            fn from_str(arg: &str) -> Result<Self, Self::Err> {
+                match &(arg.to_string())[..]{
+                    $(x if x == $disp_name.to_lowercase() => Ok($name::$val),
+                    $($alias => Ok($name::$val),)*)*
+                _ => Err(())
+                }
+            }
+        }
+
+        impl std::convert::TryFrom<usize> for $name{
+            type Error = ();
+            fn try_from(num: usize) -> Result<Self, Self::Error> {
+                match num {
+                    $(x if x == $name::$val as usize => Ok($name::$val),)*
+                    _ => Err(())
+                }
+            }
+        }
+
+        impl std::fmt::Display for $name{
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                let text = match self{
+                    $($name::$val => $disp_name,)*
+                };
+                write!(f, "{}", text)
+            }
+        }
+    }
+}
